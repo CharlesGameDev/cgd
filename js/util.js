9dfb8ab2ff92ab1490bcfcea81b410c74ceffdbb
CharlesGameDev <charlesgamedev@gmail.com>
2022-10-02 02:20:09 -0400

    oopswh

--- js/util.js
+++ js/util.js
@@ -26,17 +26,15 @@ export function clearComments() {
     }
 }
 
-export function loadComments(comments, maxlength) {
+export function loadComments(comments) {
     const loadingComments = document.getElementById("loadingcomments");
     loadingComments.innerText = ` Loading comments (0/${comments.length})`;
     clearComments();
     console.log("Loading comments (" + comments.length + ")");
     comments = comments.reverse();
-    let length = comments.length;
-    if (length > maxlength) length = maxlength;
     getLoggedUserMeta().then(snapshot => {
         const user = snapshot.val();
-        for (let i = 0; i < length; i++) {
+        for (let i = 0; i < comments.length; i++) {
             createComment(comments[i][1], comments[i][0], user);
             // loadingComments.innerText = `Loading comments (${i + 1}/${comments.length})`;
         }
